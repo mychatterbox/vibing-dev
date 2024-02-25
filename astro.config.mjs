2e3260e05ff9ae3ffc32720c85fb6f8cfd9d074c
mychatterbox <mychatterbox@gmail.com>
2024-02-25 20:05:10 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import svelte from "@astrojs/svelte";
 import markdoc from "@astrojs/markdoc";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
+import shiki from "@astrojs/markdoc/shiki";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +17,9 @@ export default defineConfig({
       filter: (p) => !p.includes("/draft/"),
     }),
   ],
+  shikiConfig: {
+    theme:  "css-variables",
+    wrap: true,
+  },
 
 });
