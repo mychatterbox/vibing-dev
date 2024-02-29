07599fc37b69a75f1e512d0dd272d42fd7fb6c6d
mychatterbox <mychatterbox@gmail.com>
2024-03-01 02:08:26 +0900

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,14 @@ import { defineCollection, z } from "astro:content";
 
 const published = defineCollection({
   schema: z.object({
-    kind: z.enum(["article", "note"]).default("note"),
+    kind: z.enum(["article", "note"]).default("article"),
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    tags: z.string().array().default([]),
+    tags: z.array(z.string()).default(["others"]),
     list: z.boolean().default(true),
+    featured: z.boolean().optional(),
+    pubDatetime: z.date().optional().nullable(),
   }),
 });
 
